5ce8f82ad15476cd6c14202ef3a66247a3a0379b
Shubham Sinha <shubham.sinha2512@gmail.com>
2022-04-29 12:57:27 +0530

    preparing for deployment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression')
+
 const AgencyRouter = require('./routes/AgencyRouter');
 const AuthRouter = require('./routes/AuthRouter');
 const ClientRouter = require('./routes/ClientRouter');
@@ -11,6 +13,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
+app.use(compression())
 
 app.use((req, res, next)=>{
     req.requestTime = new Date().toISOString();
